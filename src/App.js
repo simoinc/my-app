97c6fb64150304486dbbf27c633fff2da23adff2
simoinc <simona.inc@gmail.com>
2022-02-10 14:56:21 +0100

    Input fields integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useRef} from 'react';
+import ReactDOM from 'react-dom';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+/*
+
+export default function App() {
+  class HelloMessage extends React.Component {
+    render() {
+      return React.createElement(
+        "div",
+        null,
+        "Hello ",
+        this.props.name
+      );
+    }
+  }
+
+  ReactDOM.render(React.createElement(HelloMessage, { name: "Alex" }), document.getElementById('hello-example'));
+}
+*/
 
 function App() {
-  return (
+
+  const objectInput = useRef(null);
+  const clearInput = () =>{
+    objectInput.current.value = "";
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder='Enter your name' ref={objectInput}/>
+      <button onClick={clearInput}> Clear the field </button>
     </div>
   );
 }
