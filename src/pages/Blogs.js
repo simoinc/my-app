a086cd6331fd6986efd8208ae7f1319a8a3eacaf
simoinc <simona.inc@gmail.com>
2022-02-11 12:05:20 +0100

    added routing

---
+++ src/pages/Blogs.js
@@ -0,0 +1,7 @@
+import React, {useRef} from 'react';
+
+const Blogs = () => {
+    return <h1>Blog Articles</h1>;
+  };
+  
+  export default Blogs;
---
+++ src/pages/NoPage.js
@@ -0,0 +1,7 @@
+import React, {useRef} from 'react';
+
+const NoPage = () => {
+    return <h1>404 - Page is not found</h1>;
+  };
+  
+  export default NoPage;
